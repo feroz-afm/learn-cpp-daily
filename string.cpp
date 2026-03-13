67587dfc6e5b47e21d24ed90ddba9aa8365d84f8
Md Feroz Ahmed <ferozahmed@gmail.com>
2026-03-13 23:06:48 +0600

    add this string code

---
+++ string.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    char greeting[6]= {'H','E', 'L','L','O','\0'};
+    cout << " The Greeting message is : ";
+    cout << greeting;
+    return 0;
+ }
